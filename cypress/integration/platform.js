98dbcabd1645c1b509b2bd46f205bc538561589b
Obadha2 <w.obadha@gmail.com>
2020-09-21 16:55:45 +0300

    Implementation for Cypress.

---
+++ cypress/integration/platform.js
@@ -0,0 +1,60 @@
+describe('Platform Test', () => {
+	it('Opens Platform', () => {
+	  cy.visit('https://test-deployment.steve-buscemi.ush.zone/views/data')
+
+	  cy.contains('Log in').click({force: true})
+	  cy.get('input[id=email]').type(Cypress.env('ush_admin_email'))
+
+	  cy.get('input[id=password]').type(Cypress.env('ush_admin_pwd'))
+	  cy.get('.button-alpha').contains('Log in').click()
+
+
+	  cy.wait(3000) //wait for link to be visible
+	  cy.contains('Settings').click({force: true})
+
+	  cy.contains('General').click()
+	  cy.wait(3000)
+	  cy.go('back')
+
+	  cy.contains('Surveys').click()
+	  cy.wait(3000)
+	  cy.go('back')
+
+	  cy.contains('Data Sources').click()
+	  cy.wait(3000)
+	  cy.go('back')
+
+	  cy.contains('Import').click()
+	  cy.wait(3000)
+	  cy.go('back')
+
+	  cy.contains('Configure HDX API').click()
+	  cy.wait(3000)
+	  cy.go('back')
+
+	  cy.contains('Export and tag data').click()
+	  cy.wait(3000)
+	  cy.go('back')
+
+	  cy.contains('Users').click()
+	  cy.wait(3000)
+	  cy.go('back')
+
+	  cy.contains('Roles').click()
+	  cy.wait(3000)
+	  cy.go('back')
+
+	  cy.contains('Categories').click()
+	  cy.wait(3000)
+	  cy.go('back')
+
+	  cy.contains('Webhooks').click()
+	  cy.wait(3000)
+	  cy.go('back')
+
+	  cy.contains('Plans').click()
+	  cy.wait(3000)
+	  cy.go('back')
+
+	})
+})

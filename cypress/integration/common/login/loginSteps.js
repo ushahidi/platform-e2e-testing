8315a6a8cf842162e0e5c7a7c708fd3f25d2602b
Walter <w.obadha@gmail.com>
2021-10-23 03:14:59 +0300

    Correctly integrated cucumber. - Fixes #18

--- cypress/integration/common/login/loginSteps.js
+++ cypress/integration/common/login/loginSteps.js
@@ -1,21 +1,15 @@
 import {Given, And, Then, When} from "cypress-cucumber-preprocessor/steps"
 
+
+//for this login spec file to run from another file, it needs to be located in common folder
 Given ('Login to the application',()=>{
     cy.visit('https://test-deployment.steve-buscemi.ush.zone/views/data')
 
-	  cy.contains('Log in').click({force: true})
+	cy.contains('Log in').click({force: true})
 
-})
-
-When ('Enter valid credentials', ()=>{
     cy.get('input[id=email]').type(Cypress.env('ush_admin_email'))
     cy.get('input[id=password]').type(Cypress.env('ush_admin_pwd'))
-})
 
-Then('click Login', ()=>{
     cy.get('.button-alpha').contains('Log in').click()
-})
-
-And('See the homepage', ()=>{
-
+//add assertions to confirm user is actually logged in
 })
---
+++ cypress/integration/data_view/data_view.js
@@ -0,0 +1,23 @@
+import {Given, And, Then, When} from "cypress-cucumber-preprocessor/steps"
+
+When ('I navigate to data view page', ()=>{
+	it('Checks Data View',()=>{
+		cy.wait(3000)//this wait can be done away with if browser(viewport) window is maximized.																																																																																
+		cy.contains('Data').click({force: true})
+		cy.get('.hide-until-medium > .searchbar > .searchbar-options > .searchbar-options-filter > sort-and-filter-counter > .button-label-d > [translate=""]').contains('Sort & filter')
+		   .click({force: true})
+	})
+Then ('I should see data view information', ()=>{
+		//launch sort & filter drop-down and sort posts to show Under Review only
+		cy.wait(5000) //we have this to wait for login modal to get dismissed
+		cy.contains('Sort & filter').click({force: true})
+		cy.contains('Published').click({force: true})
+	
+		cy.contains('Apply').click({force:true})
+	
+	
+		//Changed this to check that the Save Search button exists when user alters the Filters. For now we'll leave it as
+		//check that Save Search button appears and exists.
+		cy.contains('Save Search').should('exist')
+		})
+})

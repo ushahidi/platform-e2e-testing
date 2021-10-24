dc43cd49238281d8e0da9138831f8e09e9fac64f
Walter <w.obadha@gmail.com>
2021-10-24 11:12:25 +0300

    Refactor - move survey actions to separate file

---
+++ cypress/integration/surveys/surveys.js
@@ -0,0 +1,149 @@
+import {When, Then} from "cypress-cucumber-preprocessor/steps"
+
+When ('I navigate to Surveys page',() =>{
+	cy.wait(3000)
+	cy.contains('Settings').click({force: true})
+	cy.contains('Surveys').click()
+})
+
+Then ('I can create a Survey', ()=> {
+	cy.contains('New Survey').click()
+
+	cy.get('#survey_name').type('Automated Survey')
+	cy.get('#survey_description').type('Automated Description')
+
+
+	//add survey fields
+	//title field
+	cy.get('#survey-build-post > .listing > :nth-child(2) > .listing-item-primary > .listing-item-title').click()
+	cy.get('#field-name').type('Automated Title')
+	cy.contains('Add & close').click()
+
+	//description field
+	cy.get('#survey-build-post > .listing > :nth-child(3) > .listing-item-primary > .listing-item-title').click()
+	cy.get('#field-name').type('Automated Description')
+	cy.contains('Add & close').click()
+
+	//short text field
+	cy.contains('Add field').click()
+	cy.contains('Short text').click()
+	cy.get('#field-name').type('Automated Short Text Field')
+	cy.contains('Add & close').click()
+
+	//long text field
+	cy.contains('Add field').click()
+	cy.contains('Long text').click()
+	cy.get('#field-name').type('Automated Long Text Field')
+	cy.contains('Add & close').click()
+
+	//number(decimal) field
+	cy.contains('Add field').click()
+	cy.contains('Number (Decimal)').click()
+	cy.get('#field-name').type('Automated Decimal Number Field')
+	cy.contains('Add & close').click()
+
+	//number(integer field)
+	cy.contains('Add field').click()
+	cy.contains('Number (Integer)').click()
+	cy.get('#field-name').type('Automated Number Integer Field')
+	cy.contains('Add & close').click()
+
+	//Location field
+	cy.contains('Add field').click()
+	cy.contains('Location').click()
+	cy.get('#field-name').type('Automated Location Field')
+	cy.contains('Add & close').click()
+
+	//Date Field
+	cy.contains('Add field').click()
+	cy.contains('Date').click()
+	cy.get('#field-name').type('Automated Date Field')
+	cy.contains('Add & close').click()
+
+
+	//Date & Time Field
+	cy.contains('Add field').click()
+	cy.contains('Date & Time').click()
+	cy.get('#field-name').type('Automated Date & Time Field')
+	cy.contains('Add & close').click()
+
+	//Select Field 
+	cy.contains('Add field').click()
+	cy.get('#bootstrap-app > ush-modal-container > div > div.modal-window > modal-content > survey-attribute-create > dl > dt:nth-child(15) > a').click()
+	cy.get('#field-name').type('Automated Select Field')
+	cy.contains('Add & close').click()
+
+	// Radio Field
+	cy.contains('Add field').click()
+	cy.contains('Radio Button(s)').click()
+	cy.get('#field-name').type('Automated Radio Buttons Field')
+
+	
+	cy.get('.button-secondary').click() //click Add button to add radio field
+	cy.get('[placeholder="Option 1"]').type('Radio 1') //puts a value in the radio field
+
+	cy.contains('Add & close').click()
+
+	//Checkbox Field
+	cy.contains('Add field').click()
+	cy.contains('Checkbox(es)').click()
+	cy.get('#field-name').type('Automated Checkboxes Field')
+
+	cy.get('.button-secondary').click() //click Add button to add radio field
+	cy.get('[placeholder="Option 1"]').type('Radio 1') //puts a value in the radio field
+
+	cy.contains('Add & close').click()
+
+	//Related Post Field
+	cy.contains('Add field').click()
+	cy.contains('Related Post').click()
+	cy.get('#field-name').type('Automated Related Post Field')
+	cy.contains('Add & close').click()
+
+	//Image Field
+	cy.contains('Add field').click()
+	cy.contains('Image').click()
+	cy.get('#field-name').type('Automated Image Field')
+	cy.contains('Add & close').click()
+
+	//Embed Video
+	cy.contains('Add field').click()
+	cy.contains('Embed video').click()
+	cy.get('#field-name').type('Automated Embeded Video Field')
+	cy.contains('Add & close').click()
+
+	//Categories
+	cy.contains('Add field').click()
+	cy.contains('Categories').click()
+	cy.get('#field-name').type('Automated Categories Field')
+	cy.contains('Add & close').click()
+
+
+	//Tasks
+	//opens tasks creation modal
+	cy.contains('Add task').click({force: true})
+
+	//enters values in Tasks Name and Description fields
+	cy.get('[placeholder="Name this task..."]').type('Automated Task')
+	cy.get('[placeholder="Describe this task..."]').type('Automated Task Description')
+
+	cy.contains('Add & close').click()
+
+
+	//create some task fields
+	//short text field
+	cy.get('#section-build-interim_id_3 > .listing > .listing-item > .listing-item-primary > .listing-item-title > .button').click()
+	cy.contains('Short text').click()
+	cy.get('#field-name').type('Automated Short Text Field')
+	cy.contains('Add & close').click()
+
+	//long text field
+	cy.get('#section-build-interim_id_3 > .listing > .listing-item > .listing-item-primary > .listing-item-title > .button').click()
+	cy.contains('Long text').click()
+	cy.get('#field-name').type('Automated Long Text Field')
+	cy.contains('Add & close').click()
+
+	//click Save to save survey and complete survey creation process
+	cy.contains('Save').click()
+
+})

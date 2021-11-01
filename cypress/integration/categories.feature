43b00dd2dae0efb80a535f15b33e2e1dc67ba75e
Walter <w.obadha@gmail.com>
2021-11-01 08:09:15 +0300

    Refactor - move categories to separate file

---
+++ cypress/integration/categories.feature
@@ -0,0 +1,6 @@
+Feature: Categories
+
+    Scenario: Categories Actions
+        Given Login to the application
+        When I navigate to Categories page
+        Then I can create a Category
---
+++ cypress/integration/categories/categories.js
@@ -0,0 +1,32 @@
+import {Then, When} from "cypress-cucumber-preprocessor/steps"
+
+When ('I navigate to Categories page', ()=>{
+    cy
+    .contains('Settings',{timeout: 5000})
+    .should('be.visible').click()
+
+    cy
+    .contains('Categories',{timeout: 5000})
+    .should('be.visible').click()
+
+
+})
+Then ('I can create a Category', ()=>{
+    //open category creation page
+    cy
+    .get('.button.button-alpha.button-fab')
+    .click({force: true})
+
+    //create category
+    cy
+    .get('#category-name')
+    .type('Automated Category')
+
+	cy
+    .get('#category-description')
+    .type('Automated Category Description')
+
+	cy
+    .contains('Save')
+    .click()
+})

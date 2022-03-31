b5830d43fe9415711567a914f96f1f05f44a1d7b
Walter Loso <walt@Walters-MacBook-Pro.local>
2022-03-31 10:49:49 +0300

    fix failing test on data view

--- cypress/integration/data_view/data_view.js
+++ cypress/integration/data_view/data_view.js
@@ -1,8 +1,7 @@
 import {Given, And, Then, When} from "cypress-cucumber-preprocessor/steps"
 
 When ('I navigate to data view page', ()=>{
-	it('Checks Data View',()=>{
-		cy.wait(3000)//this wait can be done away with if browser(viewport) window is maximized.																																																																																
+		cy.wait(5000)//this wait can be done away with if browser(viewport) window is maximized.																																																																																
 		cy.contains('Data').click({force: true})
 		cy.get('.hide-until-medium > .searchbar > .searchbar-options > .searchbar-options-filter > sort-and-filter-counter > .button-label-d > [translate=""]').contains('Sort & filter')
 		   .click({force: true})
@@ -20,4 +19,3 @@ Then ('I should see data view information', ()=>{
 		//check that Save Search button appears and exists.
 		cy.contains('Save Search').should('exist')
 		})
-})

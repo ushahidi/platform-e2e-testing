3d6485552bd8dd9947e230731c0ba6c4b19995c6
AmTryingMyBest <w.obadha@gmail.com>
2022-06-27 12:53:20 +0300

    verify user lands on homepage after logging in

--- cypress/integration/common/login/loginSteps.js
+++ cypress/integration/common/login/loginSteps.js
@@ -7,9 +7,19 @@ Given ('Login to the application',()=>{
 
 	cy.get('[data-cy="mb-login-page-button"]').click()
 
+    //fields not filled, verify login button disabled
+    cy.get('[data-cy="login-complete-button"]').should('be.disabled')
+
     cy.get('[data-cy="login-email-field"]').type(Cypress.env('ush_admin_email'))
     cy.get('[data-cy="login-password-field"]').type(Cypress.env('ush_admin_pwd'))
 
-    cy.get('[data-cy="login-complete-button"]').contains('Log in').click()
-//add assertions to confirm user is actually logged in
+    cy.get('[data-cy="login-complete-button"]')
+        .should('not.be.disabled')
+        .contains('Log in')
+        .click()
+
+    //add assertions to confirm user is actually logged in
+    //verify from url that user is in views page
+    cy.url()
+        .should('contain','/views/map')
 })
